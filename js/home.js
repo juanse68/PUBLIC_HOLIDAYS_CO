fd7cbbc21fe6a8bd542d8c2f9fe2c20c4e2eeaf9
juanse68 <163223814+juanse68@users.noreply.github.com>
2025-04-24 15:51:53 +0000

    13

--- js/home.js
+++
@@ -1,8 +0,0 @@
-export function render() {
-  return `
-    <section>
-      <h2>Bienvenido a Festivos Colombia 2025 🇨🇴</h2>
-      <p>Explora los días festivos y guarda tus favoritos.</p>
-    </section>
-  `;
-}
--- js/search.js
+++
@@ -1,7 +0,0 @@
-// Función para buscar un día festivo por nombre
-function searchHoliday(query) {
-  const allHolidays = JSON.parse(localStorage.getItem('holidays')) || [];  // Obtener todos los días festivos
-  return allHolidays.filter(holiday =>
-    holiday.localName.toLowerCase().includes(query.toLowerCase())  // Filtrar por nombre del día festivo
-  );
-}
